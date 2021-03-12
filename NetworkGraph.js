4f465496ec5d1a0539071f2c13a5cb8895d4be96
Raúl <raulguzmanguerrero@outlook.com>
2021-03-11 20:02:09 -0600

    Add simulation and callback function to graph

--- NetworkGraph.js
+++ NetworkGraph.js
@@ -12,7 +12,7 @@ window.onload = () => {
       { id: 0, name: "Alex Vargas" },
       { id: 1, name: "Mauricio Corona" },
     ],
-    links: [{ source: 0, target: 2 }],
+    links: [{ source: 0, target: 1 }],
   };
 
   // Aquí se está creando un nuevo elemento svg, se le está asignando características, y después se está agregando al svg un elemento g
@@ -24,7 +24,7 @@ window.onload = () => {
     .append("g") // <- Elemento que agrupa cosas dentro de un svg
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-/* Aquí se está definiendo el procedimiento general para la creación de los links del grafo. Se seleccionan todas las lineas existentes 
+  /* Aquí se está definiendo el procedimiento general para la creación de los links del grafo. Se seleccionan todas las lineas existentes 
 en el grafo y se realiza un mapeo de la selección y los datos en json.links. Para los elementos en json.links (que cuando se genere por
 primera vez el grafo van a ser todos los elementos) a los que no les corresponde un elemento, se crear una nueva linea la cual se appendea
 al grafo*/
@@ -35,7 +35,7 @@ al grafo*/
     .append("line") // <- Los elementos placeholders son reemplazados por elementos line
     .style("stroke", "#aaa");
 
-/* Aquí se realiza un proceso muy similar al realizado en link, solo que los datos son json.nodes y en lugar de usar lineas, se usan
+  /* Aquí se realiza un proceso muy similar al realizado en link, solo que los datos son json.nodes y en lugar de usar lineas, se usan
 circulos */
   let node = graph
     .selectAll("circle")
@@ -44,4 +44,37 @@ circulos */
     .append("circle")
     .attr("r", 20)
     .style("fill", "#69b3a2");
+
+/*
+  Antes de continuar, una palabra de nuestro sponsor: las fuerzas. En d3, las fuerzas son funciones que definen la posición y velocidad
+  de nodos. Un tipo de función de fuerza simple sería una que modifique la velocidad del nodo, dependiendo de la posición actual del nodo
+  y de una constante. Para más detalles, ver: https://devdocs.io/d3~5/d3-force#forces
+*/
+  let simulation = d3.forceSimulation(json.nodes) // <- Crea una nueva simulacion con el arreglo de nodos pasado como argumento y sin ninguna fuerza
+  .force( // <- Aplica la fuerza especificada, en este caso en una fuerza de tipo link. La fuerza de tipo link une o aleja dos nodos linkeados
+    "link", d3.forceLink() // <- Crea una nueva fuerza de tipo link con los links especificados en la linea 61
+    /*
+      Esta función está muy curiosa y todavía no estoy seguro de entender que esté pasando aquí. Pero según entendí, 
+      está función hace el mapeo entre cada uno de los nodos, y el link que le corresponde. La función realmente es útil si los ids de los 
+      nodos no son iguales a sus indices en el arreglo
+    */
+      .id((d) => {console.log(d); return d.id;})
+      .links(json.links) // <- Esta función es la encargada de hacer el link entre los nodos. Recordando que la referencia de a que nodo le corresponde cada link se hizo en la linea anterior
+  )
+  .force("charge", d3.forceManyBody().strength(-4000)) // <- Crea una nueva fuerza de reoulsión entre los nodos
+  .force("center", d3.forceCenter(width / 2, height / 2)) // <- Crea yba fuerza que atrae a los nodos hacia el centro
+  .on("end", ticked);
+
+  // Esta función actualiza la posición de los nodos
+  function ticked() {
+    link
+      .attr("x1", d => d.source.x)
+      .attr("y1", d => d.source.y)
+      .attr("x2", d => d.target.x)
+      .attr("y2", d => d.target.y); 
+
+    node
+      .attr("cx", d => d.x + 6)
+      .attr("cy", d => d.y - 6)
+  };
 };
